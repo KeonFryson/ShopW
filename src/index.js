710a4d1f07dd5de4ab699245961eb517aa124861
Keon Fryson <frysonk112115@gmail.com>
2026-03-25 09:45:56 -0400

    ADDED ordersDashboardHtml
    added OrderDaSHBORAD

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import shopHtml from './shop.html';
+import ordersDashboardHtml from './orders-dashboard.html';
 import { EmailMessage } from "cloudflare:email";
 
 export default {
 	async fetch(request, env) {
 		const url = new URL(request.url);
 
-		// Serve HTML page
+		// Serve HTML page for shop
 		if (url.pathname === '/') {
 			return new Response(shopHtml, {
 				headers: { 'Content-Type': 'text/html; charset=UTF-8' },
 			});
 		}
 
-		
+		// Serve orders dashboard at /order
+		if (url.pathname === '/order') {
+			return new Response(ordersDashboardHtml, {
+				headers: { 'Content-Type': 'text/html; charset=UTF-8' },
+			});
+		}
 	},
 };
